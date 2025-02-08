b8af815dd49a0106f6618c87c06685bf498659ca
Elif Alhussein <180541610@firat.edu.tr>
2025-02-08 20:27:10 +0300

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout"; 
 import Hero from "./Components/Hero";
 import About from "./Components/About";
